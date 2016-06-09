7ef98280732924ccfe1b048fe57a563c7ed71fdf
koallen <weifengzi2009@gmail.com>
2016-06-09 23:15:37 +0800

    finish forms with openID

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
+# form configurations
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'uKVD2>P2/Alz}B{1FTv|t6G2zjFI}P'
+
+# openid configurations
+OPENID_PROVIDERS = [
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
+]
