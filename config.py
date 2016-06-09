01ff3b945cf694cb406691e3bf90fc393d926832
koallen <weifengzi2009@gmail.com>
2016-06-10 01:24:48 +0800

    create and migrate sqlite db

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# sqlalchemy configurations
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
 # form configurations
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'uKVD2>P2/Alz}B{1FTv|t6G2zjFI}P'
