687851c7dc30a43d65bcd27e08c7ca46f4d76df7
koallen <weifengzi2009@gmail.com>
2016-06-09 22:13:16 +0800

    finish template

--- app/views.py
+++ app/views.py
@@ -5,6 +5,17 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'Shawn'} # a fake user
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Rainy day in Singapore.'
+        },
+        {
+            'author': {'nickname': 'Mike'},
+            'body': 'I\'ll watch the new movie tomorrow.'
+        }
+    ]
     return render_template('index.html',
                            title='Home',
-                           user=user)
+                           user=user,
+                           posts=posts)
